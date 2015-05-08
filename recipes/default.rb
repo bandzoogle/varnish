65d37a11c56942b382c34f892bf04e13933d339e
Colin Mitchell <muffinista@gmail.com>
2015-05-08 16:36:23 -0400

    i write good code

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,24 +22,24 @@ include_recipe 'varnish::repo' if node['varnish']['use_default_repo']
 
 package 'varnish'
 
-# template node['varnish']['default'] do
-#   source node['varnish']['conf_source']
-#   cookbook node['varnish']['conf_cookbook']
-#   owner 'root'
-#   group 'root'
-#   mode 0644
-#   notifies 'restart', 'service[varnish]', :delayed
-# end
-
-template "#{node['varnish']['dir']}/#{node['varnish']['vcl_conf']}" do
-  source node['varnish']['vcl_source']
-  cookbook node['varnish']['vcl_cookbook']
+template node['varnish']['default'] do
+  source node['varnish']['conf_source']
+  cookbook node['varnish']['conf_cookbook']
   owner 'root'
   group 'root'
   mode 0644
-  notifies :reload, 'service[varnish]', :delayed
-  only_if { node['varnish']['vcl_generated'] == true }
+  notifies 'restart', 'service[varnish]', :delayed
 end
+
+#template "#{node['varnish']['dir']}/#{node['varnish']['vcl_conf']}" do
+#  source node['varnish']['vcl_source']
+#  cookbook node['varnish']['vcl_cookbook']
+#  owner 'root'
+#  group 'root'
+#  mode 0644
+#  notifies :reload, 'service[varnish]', :delayed
+#  only_if { node['varnish']['vcl_generated'] == true }
+#end
 
 service 'varnish' do
   supports restart: true, reload: true
